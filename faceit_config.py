5efde588769f8a95604c413f0e32bd7a9d3dfe1b
tuntematonjr <mika.lieto@hotmail.com>
2025-09-11 10:35:42 +0300

    Update faceit_config.py

--- faceit_config.py
+++ faceit_config.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 API_KEY = os.environ.get("FACEIT_API_KEY", "").strip()
 
-CURRENT_SEASON = 11
+CURRENT_SEASON = 10
 TOOL_VERSION = 0.2
 
 # Lue .env vain jos ympäristömuuttuja puuttuu
