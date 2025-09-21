f48e45c534359a8c2235e2938957ca119093163e
tuntematonjr <mika.lieto@hotmail.com>
2025-09-22 01:02:56 +0300

    db reset

--- faceit_config.py
+++ faceit_config.py
@@ -8,8 +8,8 @@ from pathlib import Path
 
 API_KEY = os.environ.get("FACEIT_API_KEY", "").strip()
 
-CURRENT_SEASON = 11
-TOOL_VERSION = 0.3
+CURRENT_SEASON = 10
+TOOL_VERSION = 0.4
 
 # Lue .env vain jos ympäristömuuttuja puuttuu
 if not API_KEY:
--- html_gen.py
+++ html_gen.py
@@ -661,7 +661,7 @@ def topbar(show_back_to_index: bool):
       <div class="topbar">
         <div class="brand">
           <img src="https://armafinland.fi/css/gfx/armafin-logo-200px.png" alt="AFI logo" class="logo promo-logo"/>
-          <span>AFI - Unofficial Pappaliiga Stats v{TOOL_VERSION}</span>
+          <span>AFI - Unofficial Pappaliiga CS Stats v{TOOL_VERSION}</span>
           <img src="https://pappaliiga.fi/app/themes/pappaliiga/images/src/pappaliiga-logo-white-bg.png" alt="AFI logo" class="logo promo-logo"/>
         </div>
         <div class="nav">
